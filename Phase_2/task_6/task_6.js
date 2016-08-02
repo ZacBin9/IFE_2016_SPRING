59468407f26fc6c6c4500e7388510db4c762cbab
ZacBin9 <247290204@qq.com>
2016-08-02 19:36:22 +0800

    Javascript task-6

--- Phase_2/task_6/task_6.js
+++ Phase_2/task_6/task_6.js
@@ -1,3 +1,4 @@
+//跨浏览器的绑定事件函数
 function addHandler(element, type, handler){
   if (element.addEventListener){
     element.addEventListener(type, handler, false);
@@ -9,6 +10,7 @@ function addHandler(element, type, handler){
 }
 var queneWrapper = document.getElementById('quene');
 var buttonGroup = document.getElementsByTagName('input');
+//建立队列对象
 var queneObj = {
   queneArray : [],
   leftIn : function(value){
@@ -19,8 +21,26 @@ var queneObj = {
     this.queneArray.push(value);
     this.render();
   },
-  leftOut : function(){},
-  rightOut : function(){},
+  leftOut : function(){
+    if (this.queneArray.length > 0){
+      alert(this.queneArray.shift());
+      this.render();
+    }else {
+      alert('队列已空,不能再出啦');
+    }
+  },
+  rightOut : function(){
+    if (this.queneArray.length > 0){
+      alert(this.queneArray.pop());
+      this.render();
+    }else {
+      alert('队列已空,不能再出啦');
+    }
+  },
+  deleteValue : function(index){
+    this.queneArray.splice(index, 1);
+    this.render();
+  },
   render : function(){
     var queneStr = '';
     this.queneArray.forEach(function(item){
@@ -29,19 +49,41 @@ var queneObj = {
     queneWrapper.innerHTML = queneStr;
   }
 };
+
+//绑定事件
 addHandler(document, 'click', function(event){
-  var valueInput = document.getElementById('value-input').value;
   var target = event.target;
+  //点击元素删除事件
+  if (target.parentNode.id === 'quene') {
+    var index = 0;
+    while (target !== queneWrapper.children[index]){
+      index++;
+    }
+    queneObj.deleteValue(index);
+    return;
+  }
+  //四个按钮的时间
+  var valueInput = document.getElementById('value-input').value;
   switch (target.id) {
     case 'left-in':
-      queneObj.leftIn(valueInput);
+      if((/^[0-9]+.?[0-9]*$/).test(valueInput)) {
+        queneObj.leftIn(parseFloat(valueInput));
+      }else {
+        alert('请输入有效的数字');
+      }
       break;
     case 'right-in':
-      queneObj.rightIn(valueInput);
+      if((/^[0-9]+.?[0-9]*$/).test(valueInput)) {
+        queneObj.rightIn(parseFloat(valueInput));
+      }else {
+        alert('请输入有效的数字');
+      }
       break;
     case 'left-out':
+      queneObj.leftOut();
       break;
     case 'right-out':
+      queneObj.rightOut();
       break;
     default:
       break;
